5c29dd05f8403b32e4dfb14d04920e9f7f4eda62
Liz Liu <liangliang0628@gmail.com>
2025-02-18 12:40:34 -0500

    feat: make CardList component dynamic

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -1,14 +1,32 @@
 import Card from './Card';
 
+
 function CardList({robots}){
-  return(
-     <div>
-        <Card id ={robots[0].id} name ={robots[0].name} email = {robots[0].email}/>
-        <Card id ={robots[1].id} name ={robots[1].name} email = {robots[1].email}/>
-        <Card id ={robots[2].id} name ={robots[2].name} email = {robots[2].email}/>
-      </div>
-  )
+  robots.map((user,i) => {
+    return(
+       <div>
+          <Card 
+          key = {i}
+          id ={user.id} 
+          name ={user.name} 
+          email = {user.email}
+          />
+        </div>
+    )
+  })
 }
+
+// function CardList({robots}){
+//   return(
+//      <div>
+//         <Card id ={robots[0].id} name ={robots[0].name} email = {robots[0].email}/>
+//         <Card id ={robots[1].id} name ={robots[1].name} email = {robots[1].email}/>
+//         <Card id ={robots[2].id} name ={robots[2].name} email = {robots[2].email}/>
+//       </div>
+//   )
+// }
 export default CardList;
+
+
 
 
